78253a5ebaca9cb58ba4fe80e4ac28e89c083749
Ryan Mayobre <rbmayobre@protonmail.com>
2023-01-26 19:49:10 -0500

    Added missing files to theme package

---
+++ packages/theme/lib/theme.dart
@@ -0,0 +1,7 @@
+library theme;
+
+import 'src/color_scheme.dart';
+import 'src/theme_data.dart';
+
+final darkThemeData = baseThemeData(colorScheme: darkColorScheme);
+final lightThemeData = baseThemeData(colorScheme: lightColorScheme);
